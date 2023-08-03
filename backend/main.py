ed9332e954d10fbd9c92e40c5c8ff4aefa793396
shadmeoli <shadcodes@gmail.com>
2023-08-03 17:16:19 +0300

    update: new setup

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,22 @@
+import logging
+from datetime import datetime
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
+app = FastAPI(
+    title="Some pet bot",
+    description="I am just extending on a pet project\nNothing sirious",
+    version="0.2.0",
+    redoc_url="/redoc",
+    contact={"name": "Shadrack Meoli"},
+)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[*],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,3 +29,12 @@ async def to_nothing():
     return {
         "Nothing" : "Here"
     }
+
+
+
+
+if __name__ == "__main__":
+    try:
+        uvicorn.run("main:app", host="0.0.0.0", reload=True)
+    except Exception as err:
+        logging.error(err)
