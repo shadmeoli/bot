95ebf45c2bf269904d2eec05b5c37fcc4a43eb1d
shadmeoli <shadcodes@gmail.com>
2023-08-03 17:37:32 +0300

    update: new setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
 
 const app = createApp(App)
 
