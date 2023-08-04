b2c826f22a491b14a018e125e26a8b26736f331e
shadmeoli <shadcodes@gmail.com>
2023-08-04 12:21:54 +0300

    update: chat API connection with store setup

--- frontend/src/stores/chat.ts
+++ frontend/src/stores/chat.ts
@@ -4,10 +4,11 @@ import axios from 'axios';
 
 export const useChatStore = defineStore('chat', () => {
   const count = ref(0)
+  const userText = ref<string>();
   const replies = ref<string[]>([]);
   const prompts = ref<string[]>([]);
 
-  const fetchReplies = async (prompt: string) => {
+  const fetchReplies = async () => {
 
     const apiEndpoint = 'http://0.0.0.0:8000/api/v1/chat';
 
@@ -21,8 +22,9 @@ export const useChatStore = defineStore('chat', () => {
       const reply = response.data.bot;
 
       // Add the fetched reply to the replies buffer
-      prompts.value.push(prompt);
+      prompts.value.push(userText.value);
       replies.value.push(reply);
+      userText.value = '';
 
     } catch (error) {
       console.error('Error fetching replies:', error);
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
+import { ref, onMounted, computed } from 'vue'
+import { useChatStore } from '../stores/chat';
 
+const chatStore = useChatStore()
+const messages = ref<{ id: string; text: string; isPrompt: boolean }[]>([])
+
+const fetchReplies = async () => {
+  const prompt: string = chatStore.userText.value // Add your prompt data here
+  await chatStore.fetchReplies()
+  messages.value = [...chatStore.prompts, ...chatStore.replies].map((message, index) => ({
+    id: `message_${index}`,
+    text: message,
+    isPrompt: index < chatStore.prompts.length,
+  }))
+}
+
+// Fetch replies when the component is mounted (optional)
+onMounted(fetchReplies)
 </script>
 
 <template>
@@ -13,15 +30,26 @@
         
         <body class="w-screen h-screen flex flex-col items-center mt-20  md:mt-40">
           <h1 class="font-bold">Chat Window</h1>
-          <div class="bg-gray-200 h-[50%] w-[80%] p-4 flex flex-col-reverse justify-end overflow-y-scroll" id="chatWindow">
+          <!-- <div class="bg-gray-200 h-[50%] w-[80%] p-4 flex flex-col-reverse justify-end overflow-y-scroll" id="chatWindow">
             <h1 class="flex justify-center mt-40 md:text-4xl font-bold text-gray-300">Chats will apper here ...</h1>
             <div class="mb-4">User: Hello!</div>
             <div class="mb-4">Bot: Hi there!</div>
+          </div> -->
+          <div class="bg-gray-200 h-[50%] w-[80%] p-4 flex flex-col-reverse justify-end overflow-y-scroll" id="chatWindow">
+
+            <div v-for="message in messages" :key="message.id" class="mb-4">
+              <div v-if="message.isPrompt">
+                User: {{ message.text }}
+              </div>
+              <div v-else>
+                Bot: {{ message.text }}
+              </div>
+            </div>
           </div>
 
           <div class="flex w-[80%] h-28 bg-black p-4 items-center">
             <input class="outline-none active:outline-none hover:outline-none w-[90%] h-14 md:h-20 bg-gray-200 px-4 font-bold text-black" placeholder="enter text here">
-            <button class="h-14 md:h-20 w-40 bg-gradient-to-br from-gray-400 to-gray-300 hover:from-gray-600 hover:to-gray-500 active:from-gray-400 active:to-gray-300 font-bold text-white">SEND</button>
+            <button @click="fetchReplies" class="h-14 md:h-20 w-40 bg-gradient-to-br from-gray-400 to-gray-300 hover:from-gray-600 hover:to-gray-500 active:from-gray-400 active:to-gray-300 font-bold text-white">SEND</button>
           </div>
         </body>
 
