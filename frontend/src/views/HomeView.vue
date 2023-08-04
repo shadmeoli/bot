5f9bd93d2a7cc156e804179ad8b0700f111aedd8
shadmeoli <shadcodes@gmail.com>
2023-08-04 11:43:45 +0300

    update: chat API connection with store setup

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,18 +11,17 @@
           <img src="https://shopzetu.com/cdn/shop/files/Logo_for_white_Background_100x.png?v=1672727778" alt="">
         </nav>
         
-        <body class="w-screen h-screen flex flex-col items-center justify-center">
+        <body class="w-screen h-screen flex flex-col items-center mt-20  md:mt-40">
           <h1 class="font-bold">Chat Window</h1>
           <div class="bg-gray-200 h-[50%] w-[80%] p-4 flex flex-col-reverse justify-end overflow-y-scroll" id="chatWindow">
-            <h1 class="flex justify-center mt-40 text-4xl font-bold text-gray-300">Chats will apper here ...</h1>
+            <h1 class="flex justify-center mt-40 md:text-4xl font-bold text-gray-300">Chats will apper here ...</h1>
             <div class="mb-4">User: Hello!</div>
             <div class="mb-4">Bot: Hi there!</div>
           </div>
 
-
-          <div class="flex w-[80%] h-28 bg-black p-4">
-            <input class="rounded-l-xl w-[80%] h-20 bg-gray-200 px-4 font-bold text-black" placeholder="enter text here">
-            <button class="h-20 w-40 bg-gradient-to-br from-red-400 to-orange-300 hover:from-red-600 hover:to-orange-500 active:from-red-400 active:to-orange-300 rounded-r-xl font-bold text-white">SEND</button>
+          <div class="flex w-[80%] h-28 bg-black p-4 items-center">
+            <input class="outline-none active:outline-none hover:outline-none w-[90%] h-14 md:h-20 bg-gray-200 px-4 font-bold text-black" placeholder="enter text here">
+            <button class="h-14 md:h-20 w-40 bg-gradient-to-br from-gray-400 to-gray-300 hover:from-gray-600 hover:to-gray-500 active:from-gray-400 active:to-gray-300 font-bold text-white">SEND</button>
           </div>
         </body>
 
