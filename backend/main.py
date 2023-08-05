5e0546ec35fd6a417bdea5a93db0f4ff9fe229fb
shadmeoli <shadcodes@gmail.com>
2023-08-05 19:52:05 +0300

    fix: methods allowed on the API

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
