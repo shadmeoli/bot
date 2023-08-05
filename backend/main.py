d1c05b50482a3fb62234e8ad954f1d136df70d11
shadmeoli <shadcodes@gmail.com>
2023-08-05 16:23:09 +0300

    adv: bot cleanup

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ORIGINS],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT"],
     allow_headers=["*"],
--- frontend/src/stores/chat.ts
+++ frontend/src/stores/chat.ts
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import axios from 'axios';
 
 export const useChatStore = defineStore('chat', () => {
   const count = ref(0)
@@ -17,7 +17,7 @@ export const useChatStore = defineStore('chat', () => {
     try {
       // Make the API call using Axios
       const response = await axios.post(apiEndpoint, {
-        message: userText.value,
+        message: text,
       });
 
       // Assuming your API response contains the 'bot' field with the reply text
